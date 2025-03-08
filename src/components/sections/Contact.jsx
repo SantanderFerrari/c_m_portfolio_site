5557459faf3f44ab1ed340471a7ca8f2e7d59e15
SantanderFerrari <daniel.wako98@gmail.com>
2025-03-08 18:05:13 +0300

    Update contact form

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -3,7 +3,7 @@ import { RevealOnScroll } from "../RevealOnScroll";
 import emailjs from "emailjs-com";
 
 export const Contact = () => {
-  const [formData, sentFormData] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
@@ -21,7 +21,7 @@ export const Contact = () => {
       )
       .then((result) => {
         alert("Message Sent!");
-        sentFormData({ name: "", email: "", message: "" });
+        setFormData({ name: "", email: "", message: "" });
       })
       .catch(() => alert("Oops! Something went wrong. Please try again."));
   };
@@ -32,9 +32,9 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
-          <h2 className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {""}
+        <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
             Get In Touch
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
@@ -45,10 +45,10 @@ export const Contact = () => {
                 name="name"
                 required
                 value={formData.name}
-                className="w-full bg-white/5 border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="Name..."
                 onChange={(e) =>
-                  sentFormData({ ...formData, name: e.target.value })
+                  setFormData({ ...formData, name: e.target.value })
                 }
               />
             </div>
@@ -60,28 +60,29 @@ export const Contact = () => {
                 name="email"
                 required
                 value={formData.email}
-                className="w-full bg-white/5 border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="example@gmail.com..."
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="example@gmail.com"
                 onChange={(e) =>
-                  sentFormData({ ...formData, email: e.target.value })
+                  setFormData({ ...formData, email: e.target.value })
                 }
               />
             </div>
 
             <div className="relative">
-              <input
+              <textarea
                 id="message"
                 name="message"
                 required
                 rows={5}
                 value={formData.message}
-                className="w-full bg-white/5 border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="Your Message..."
                 onChange={(e) =>
-                  sentFormData({ ...formData, message: e.target.value })
+                  setFormData({ ...formData, message: e.target.value })
                 }
               />
             </div>
+
             <button
               type="submit"
               className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
